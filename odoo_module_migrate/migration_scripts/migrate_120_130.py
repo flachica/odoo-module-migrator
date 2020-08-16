d83fd529c4971ffb099a16b34a4ec215f600c37f
Fernando La Chica <fernando.lachica@guadaltech.es>
2020-08-16 12:20:26 +0200

    Refactoring script into classes. ATTENTION details
        * Module script is converted to instance of class
        * It is easy to convert, only need move the current content of script to __init__ class
        * All script have the run base function that operates with protected properties _TEXT_REPLACES, ERRORS, ETC. `def run(self, module_path, manifest_path, module_name, migration_steps, directory_path, commit_enabled)`
        * Also have `def process_file(self, root, filename, extension, file_renames, directory_path, commit_enabled)` to process file one by one
        * If a script fail you can know so easy. Only need review the stack trace.
        * On this refactoring you could delete global functions. Only you need inherit run method and overwrite it
        * I have maintain global_funtions

--- odoo_module_migrate/migration_scripts/migrate_120_130.py
+++ odoo_module_migrate/migration_scripts/migrate_120_130.py
@@ -1,39 +1,43 @@
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo_module_migrate.base_migration_script import BaseMigrationScript
 
-_TEXT_ERRORS = {
-    "*": {
-        "web_settings_dashboard":
-        "[V13] Reference to 'web_settings_dashboard'"
-        ". This module has been removed."
-    },
-    ".py": {
-        r".*@api.returns.*\n":
-        "[13] Use of deprecated decorator '@api.returns'",
-        r".*@api.cr.*\n":
-        "[13] Use of deprecated decorator '@api.cr'",
-        r".*@api.model_cr.*\n":
-        "[13] Use of deprecated decorator '@api.model_cr'",
-    },
-}
 
-_TEXT_REPLACES = {
-    ".py": {
-        r".*@api.multi.*\n": "",
-        r".*@api.one.*\n": "",
-        r"\.sudo\((?P<user>[^/)]+?)\)": r".with_user(\g<user>)",
-        r"\.suspend_security": ".sudo",
-        r"\"base_suspend_security\",\n": "",
-        r"self.env.user.company_id": r"self.env.company"
-    },
-    ".xml": {
-        r"( |\t)*<field name=('|\")view_type('|\")>.*</field>\n": "",
-        r"( |\t)*<field name=('|\")domain_force('|\")>"
-        r".*company_id.*child_of.*</field>\n":
-            r"\t\t<field name=\2domain_force\2>"
-            r"['|',('company_id','=',False),"
-            r"('company_id', 'in', company_ids)]</field>\n",
-        r"src_model": "binding_model"
-    }
-}
+class MigrationScript(BaseMigrationScript):
+    def __init__(self):
+        self._TEXT_ERRORS = {
+            "*": {
+                "web_settings_dashboard":
+                "[V13] Reference to 'web_settings_dashboard'"
+                ". This module has been removed."
+            },
+            ".py": {
+                r".*@api.returns.*\n":
+                "[13] Use of deprecated decorator '@api.returns'",
+                r".*@api.cr.*\n":
+                "[13] Use of deprecated decorator '@api.cr'",
+                r".*@api.model_cr.*\n":
+                "[13] Use of deprecated decorator '@api.model_cr'",
+            },
+        }
+
+        self._TEXT_REPLACES = {
+            ".py": {
+                r".*@api.multi.*\n": "",
+                r".*@api.one.*\n": "",
+                r"\.sudo\((?P<user>[^/)]+?)\)": r".with_user(\g<user>)",
+                r"\.suspend_security": ".sudo",
+                r"\"base_suspend_security\",\n": "",
+                r"self.env.user.company_id": r"self.env.company"
+            },
+            ".xml": {
+                r"( |\t)*<field name=('|\")view_type('|\")>.*</field>\n": "",
+                r"( |\t)*<field name=('|\")domain_force('|\")>"
+                r".*company_id.*child_of.*</field>\n":
+                    r"\t\t<field name=\2domain_force\2>"
+                    r"['|',('company_id','=',False),"
+                    r"('company_id', 'in', company_ids)]</field>\n",
+                r"src_model": "binding_model"
+            }
+        }
